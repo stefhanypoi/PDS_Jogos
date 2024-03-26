26037bb827e3e94990d66ee7d5e8eb169a476265
Stefhany Poi <130561759+stefhanypoi@users.noreply.github.com>
2024-03-26 09:28:42 -0300

    forvca p1

---
+++ forca.py
@@ -0,0 +1,9 @@
+def jogar ():
+    print("*********************************")
+    print("***Bem vindo ao jogo da Forca!***")
+    print("*********************************")
+    
+    palavra_secreta = "maça".upper()
+    letras_acertadas = ["_" for letra in palavra_secreta]
+    letras_
+    
